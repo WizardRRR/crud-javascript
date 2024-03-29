80a12c9c87afee27edd028ee53acd995d3051b1b
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-03-29 00:16:49 -0500

    Solving remarks

--- js/features.js
+++ js/features.js
@@ -1,26 +1,26 @@
-import { deleteUser, getAllUsers, getUserById } from "./services.js";
-import { MODES_FORM } from "./mode-forms.js";
-import { $, $$ } from "./jquery.js";
+import { deleteUser, getAllUsers, getUserById } from './services.js'
+import { MODES_FORM } from './mode-forms.js'
+import { $, $$ } from './jquery.js'
 
 export function resetFields() {
-  $("#name").value = "";
-  $("#lastName").value = "";
-  $("#age").value = "";
-  $("#city").value = "";
-  $("#color").value = "";
-  $("#urlImage").value = "";
+  $('#name').value = ''
+  $('#lastName').value = ''
+  $('#age').value = ''
+  $('#city').value = ''
+  $('#color').value = ''
+  $('#urlImage').value = ''
 }
 
 export function updateUI() {
-  let templateHMTL = ``;
+  let templateHMTL = ``
   getAllUsers().forEach((user) => {
-    let url = "";
-    if (user.urlImage && user.urlImage.startsWith("https://")) {
-      url = `<img width=250 src='${user.urlImage}' alt='${user.name}'s Image' />`;
+    let url = 'Puedes Agregar una imágen!'
+    if (user.urlImage && user.urlImage.startsWith('https://')) {
+      url = `<img width=250 src='${user.urlImage}' alt='${user.name}'s Image' />`
     }
     if (user.deletedAt === null) {
       templateHMTL += `
-      <div style="border-left: 10px solid ${user.color}" id='${user.id}'>
+      <div style='border-left: 10px solid ${user.color}' id='${user.id}'>
         <span>${user.name} ${user.lastName}</span>
         <span>${user.age}</span>
         <span>${user.city}</span>
@@ -32,41 +32,41 @@ export function updateUI() {
           <img width=25 height=25 src='./assets/icon-delete.svg'/>
         </button>
       </div>
-      `;
+      `
     }
-  });
-  $("#wrapped-users").innerHTML = templateHMTL;
+  })
+  $('#wrapped-users').innerHTML = templateHMTL
 }
 
 export function addListenersButton() {
-  $$(".button-delete").forEach((buttonDelete) => {
-    $(`#${buttonDelete.id}`).addEventListener("click", () => {
-      deleteUser(parseInt(buttonDelete.id.split("-")[1]));
-      updateUI();
-      resetFields();
-      addListenersButton();
-      $("#btn-save-user").style.display = "block";
-      $("#form-users").setAttribute("mode", MODES_FORM.save);
-      $("#btn-update-user").style.display = "none";
-      $("#name").focus();
-    });
-  });
+  $$('.button-delete').forEach((buttonDelete) => {
+    $(`#${buttonDelete.id}`).addEventListener('click', () => {
+      deleteUser(parseInt(buttonDelete.id.split('-')[1]))
+      updateUI()
+      resetFields()
+      addListenersButton()
+      $('#btn-save-user').style.display = 'block'
+      $('#form-users').setAttribute('mode', MODES_FORM.save)
+      $('#btn-update-user').style.display = 'none'
+      $('#name').focus()
+    })
+  })
 
-  $$(".button-edit").forEach((buttonEdit) => {
-    $(`#${buttonEdit.id}`).addEventListener("click", () => {
-      const currentUser = getUserById(parseInt(buttonEdit.id.split("-")[1]));
+  $$('.button-edit').forEach((buttonEdit) => {
+    $(`#${buttonEdit.id}`).addEventListener('click', () => {
+      const currentUser = getUserById(parseInt(buttonEdit.id.split('-')[1]))
       // seleccionando inputs
-      $("#name").value = currentUser.name;
-      $("#lastName").value = currentUser.lastName;
-      $("#age").value = currentUser.age;
-      $("#city").value = currentUser.city;
-      $("#color").value = currentUser.color;
-      $("#urlImage").value = currentUser.urlImage;
-      $("#btn-update-user").setAttribute("user-id", currentUser.id);
-      $("#btn-save-user").style.display = "none";
-      $("#btn-update-user").style.display = "block";
-      $("#form-users").setAttribute("mode", MODES_FORM.update);
-      $("#name").focus();
-    });
-  });
+      $('#name').value = currentUser.name
+      $('#lastName').value = currentUser.lastName
+      $('#age').value = currentUser.age
+      $('#city').value = currentUser.city
+      $('#color').value = currentUser.color
+      $('#urlImage').value = currentUser.urlImage
+      $('#btn-update-user').setAttribute('user-id', currentUser.id)
+      $('#btn-save-user').style.display = 'none'
+      $('#btn-update-user').style.display = 'block'
+      $('#form-users').setAttribute('mode', MODES_FORM.update)
+      $('#name').focus()
+    })
+  })
 }
