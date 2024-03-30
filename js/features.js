c8e245853c0592f426d22823ae30d906908f9f19
ANGELEDU141 <angel.abamto@gmail.com>
2024-03-30 17:07:56 -0500

    Correction details

--- js/features.js
+++ js/features.js
@@ -29,11 +29,15 @@ export function updateUI() {
   $('#wrapped-users').innerHTML = templateHMTL
 }
 
+
+
 export function addListenersButton() {
 
   $$('.button-delete').forEach(buttonDelete => {
     $(`#${buttonDelete.id}`).addEventListener('click', () => {
-      $(`#${buttonDelete.id}`).parentNode.classList.add('deleted');
+      $(`#${buttonDelete.id}`).parentNode.classList.add('deleted')
+      const parentElement = $(`#${buttonDelete.id}`).parentNode.parentNode;
+    
       setTimeout(() => {
       deleteUser(parseInt(buttonDelete.id.split('-')[1]))
       updateUI()
@@ -43,13 +47,20 @@ export function addListenersButton() {
       $('#form-users').setAttribute('mode', MODES_FORM.save)
       $('#btn-update-user').style.display = 'none'
       $('#name').focus()
+      $(`#${buttonDelete.id}`).classList.remove('deleted');
+  
+   
     }, 300); 
-    })
+   
+    });
+    
+    
   })
 
   $$('.button-edit').forEach((buttonEdit) => {
     $(`#${buttonEdit.id}`).addEventListener('click', () => {
       const currentUser = getUserById(parseInt(buttonEdit.id.split('-')[1]))
+      
       // seleccionando inputs
       $('#name').value = currentUser.name
       $('#lastName').value = currentUser.lastName
